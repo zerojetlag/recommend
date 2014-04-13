d2fcd5ec9b6386bb33746e7795724b5b8bb1b976
sean <sean@3cps.ca>
2014-04-14 01:03:12 +0800

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myapp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  root :to => 'books#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
